ecebd7b851a9efa54d9ef5aab80d0c9a0772fe32
Karl Sigel <heykarl@gmail.com>
2021-10-21 12:04:52 -0700

    added styling to about us page, and top section of home page. Added styling to page up/down buttons. Added bio info and photo for karl

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -4,15 +4,22 @@ import { creators } from './CreatorsData.js'
 export default class AboutUs extends Component {
     render() {
         return (
+            <section className='bios-page'>
+            <h1>TEAM PARKS</h1>
             <section className='bios'>
                 {creators.map(creator => 
                     <span key={creator.name}>
                     <img src={creator.image} alt={creator.name}/>
-                    <div>{creator.name}</div>
-                    <div>{creator.bio}</div>
+                    <section>
+                    <p>{creator.name}</p>
+                    <p>{creator.title}</p>
+                    <p>{creator.bio}</p>
+                    <p>{creator.park}</p>
+                    </section>
                     </span>
                 )
                 }
+                </section>
             </section>
         )
     }
